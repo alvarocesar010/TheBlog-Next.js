fe001bb77431b6c9418534a70b7e42365c21442f
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-06 16:43:51 +0100

    feat(app): Implement loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import SpinLoader from "@/components/SpinLoader";
+import React from "react";
+
+const loadingRoot = () => {
+  return <SpinLoader containerClasses="min-h-screen " />;
+};
+export default loadingRoot;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import SpinLoader from "@/components/SpinLoader";
+import { postRepository } from "@/repositories/post";
 
 export default async function HomePage() {
+  const posts = await postRepository.findAll();
+
   return (
     <div>
-      <SpinLoader containerClasses="min-h-[250px]" />
+      {posts.map((post) => {
+        return <p key={post.id}>{post.title}</p>;
+      })}
     </div>
   );
 }
