9439aa8eb85d79bc68656ce561d2f7206339d495
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-06 17:19:10 +0100

    refactor(loading) refactored the loading for the PostList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+import PostsList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
-import { postRepository } from "@/repositories/post";
+import { Suspense } from "react";
 
 export default async function HomePage() {
-  const posts = await postRepository.findAll();
-
   return (
     <div>
-      {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
-      })}
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8 ">
+          Aqui é a Header
+        </h1>
+      </header>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+      <footer>
+        <p className="text-6xl font-bold text-center py-8 ">Footer</p>
+      </footer>
     </div>
   );
 }
