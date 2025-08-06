da693af6175a4178b5c042d0093f7f952c441f9b
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-06 18:04:35 +0100

    feat(Container): Implement the container in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Container from "@/components/Container";
 import PostsList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8 ">
           Aqui é a Header
@@ -16,6 +17,6 @@ export default async function HomePage() {
       <footer>
         <p className="text-6xl font-bold text-center py-8 ">Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
