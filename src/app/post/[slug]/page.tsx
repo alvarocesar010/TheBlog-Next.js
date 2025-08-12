1a4811824e1dee1035446d5d93cca1a2ab39527a
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-12 15:59:21 +0100

    feat(app): add generateMetadata for SEO in post/[slug]

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,24 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
-const PostSlugPage = async ({ params }: PostSlugPageProps) => {
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+    alternates: { canonical: `localhost:3000/post/${post.slug}` },
+  };
+}
+
+export const PostSlugPage = async ({ params }: PostSlugPageProps) => {
   const { slug } = await params;
 
   const post = await findPostBySlugCached(slug);
