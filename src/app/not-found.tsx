c3d88320dd4461eb03430b7d2f734a4887ade28c
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-12 13:51:58 +0100

    feat(app): Implement the not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+const NotFoundPage = () => {
+  return (
+    <div
+      className="
+    min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8
+    rounded-xl flex items-center justify-center text-center
+    "
+    >
+      <div>
+        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+        <p>Error 404 - The page you are trying access does not exist</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
